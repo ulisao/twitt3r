5f99b76043a7e7c6fa36ccbfd4e0e3f0e8d3d13a
ulisao <ulisessbaretta@gmail.com>
2022-12-05 10:48:05 -0300

    index mod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Timeline />
+        <Timeline where={ undefined }  />
       </div>
     </>
   );
